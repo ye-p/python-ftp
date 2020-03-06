42d17fde8d595b9f395207556921ec47c37a02ec
yuki-yoshizawa <yoshizawa@ye-p.co.jp>
2020-03-06 04:02:16 +0000

    Update refactoring  classification

--- classftp.py
+++ classftp.py
@@ -4,20 +4,54 @@ import pdb
 from ftplib import FTP
 from setup.setup import *
 
-# 対象のホストにアクセスしてログイン
-ftp = FTP(
-    host,
-    username,
-    password
-)
-
 # class化
-class ftpUpload():
+class FtpUpload():
 
-    # サーバー上のカレントディレクトリの一覧を取得
-    print(os.getcwd())
+    # 対象のホストにアクセスしてログイン
+    ftp = FTP(
+        host,
+        username,
+        password
+    )
     myPath = basepath
 
-# FTPサーバとの切断
-ftp.quit()
+    # サーバー上のカレントディレクトリの一覧を取得
+    # print(os.getcwd())
+
+    def echoPath(self):
+        print self.myPath
+
+    def nowDir(self):
+        print self.ftp.dir()
+
+    def uploadInFTP(self):
+        files = os.listdir(self.myPath)
+        os.chdir(self.myPath)
+
+        for f in files:
+            # 対象のファイル・ディレクトリがなければ作成
+           if os.path.isfile(self.myPath + r'/{}'.format(f)):
+               fh = open(f, 'rb')
+               self.ftp.storbinary('STOR %s' % f, fh)
+               fh.close()
+           elif os.path.isdir(self.myPath + r'/{}'.format(f)):
+               self.ftp.mkd(f)
+               self.ftp.cwd(f)
+               # ディレクトリ配下のファイル・ディレクトリもアップロード
+               uploadInFTP(self.myPath + r'/{}'.format(f))
+        self.ftp.cwd('..')
+        self.ftp.dir()
+        os.chdir('..')
+
+        #uploadInFTP(self.myPath)
+
+    # FTPサーバとの切断
+    def quit(self):
+        self.ftp.quit
+
+ftpUpload = FtpUpload()
+ftpUpload.echoPath()
+ftpUpload.uploadInFTP()
+ftpUpload.nowDir()
+ftpUpload.quit()
 
