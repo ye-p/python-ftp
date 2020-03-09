1687e8ec52c578192e8b6c40082c60d4b891c12f
yuki-yoshizawa <yoshizawa@ye-p.co.jp>
2020-03-09 10:19:58 +0000

    途中

--- classftp.py
+++ classftp.py
@@ -1,58 +1,61 @@
 # -*- coding: utf-8 -*-
 import os
 import pdb
+# ftpモジュールを読み込む
 from ftplib import FTP
 from setup.setup import *
 
-class FtpUpload():
+class FtpUpload:
 
-    # 対象のホストにアクセスしてログイン
-    ftp = FTP(
-        host,
-        username,
-        password
-    )
+    # 他の関数でも使いたいものをクラス変数化
+    ftp = FTP(host,username,password) # 対象のホストにアクセスしてログイン
     files = os.listdir(basepath)
-    uploadPath = basepath + '/{}'.format(files)
-
-    def echoPath(self):
-        print basepath
-
+ 
+    # サーバー上のカレントディレクトリの一覧を取得
     def nowDir(self):
         print self.ftp.dir()
 
-    def isFile(self):
-        if os.path.isfile(self.uploadPath):
-            fh = open(self.files, 'rb')
-            ftp.storbinary('STOR %s' % self.files, fh)
-            fh.close()
-
-    def isDir(self):
-        if os.path.isdir(self.uploadPath):
-            self.ftp.mkd(self.files)
-            self.ftp.cwd(self.files)
-
-    def FTPUpload(uploadFile,uploadDir):
-        os.chdir(basepath)
+    # 対象のファイル・ディレクトリがなければ作成
+    def uploadDir(self):
         for f in self.files:
-            print f
-            return uploadFile(f)
-            return UploadDir(f)
+            file = f
+            #print file
+            def isDir(file):
+                print os.path.isdir(basepath + '/{}'.format(file))
 
-            #    ディレクトリ配下のファイル・ディレクトリもアップロード
-            #    uploadInFTP(self.uploadPath)
+                return isDir(file)
+                def mkd(self):
+                    self.ftp.mkd(f)
+                    self.ftp.cwd(f)
+                    print self.ftp.dir()
+                    # ディレクトリ配下のファイル・ディレクトリもアップロード
+                    #uploadDir(basepath + '/{}'.format(f))
+#            elif os.path.isfile(basepath + '/{}'.format(f)):
+#        #        fh = open(f, 'rb')
+#        #        self.ftp.storbinary('STOR %s' % f, fh)
+#        #        fh.close()
+#        self.ftp.dir()
+        
+    # 対象のディレクトリに移動
+    # ファイルを転送する
 
     # FTPサーバとの切断
     def quit(self):
         self.ftp.quit
 
-    #print uploadInFTP(getFiles)
 
-ftpUpload = FtpUpload()
-#ftpUpload.echoPath()
-ftpUpload.isFile()
-ftpUpload.isDir()
-ftpUpload.FTPUpload(isFile,isDir)
-#ftpUpload.nowDir()
-ftpUpload.quit()
+# メソッドチェーンで呼び出す
+ftp = FtpUpload()
+
+# カレントディレクトリの一覧を表示
+#ftp.nowDir()
+
+# 対象のディレクトリがなければ作成
+# 1. 対象のディレクトリを確認
+#ftp.isDir()
+# 2. 対象のディレクトリを作成(アップロード)
+ftp.uploadDir()
+
+# FTPサーバとの切断
+ftp.quit()
 
