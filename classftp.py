37556c560d76b8f655f371e92bdf6740e2eda02b
yuki-yoshizawa <yoshizawa@ye-p.co.jp>
2020-03-13 11:06:27 +0000

    本日対応分

--- classftp.py
+++ classftp.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-# 設定ファイルを読み込む
+# モジュールを読み込む
 import os
-from setup.setup import *
 # FTPモジュールを読み込む
 from ftplib import FTP
+# 設定ファイルを読み込む
+from setup.setup import *
 
 class ftp:
 
@@ -21,50 +22,41 @@ class ftp:
     def isDirectry(self, directryPath):
         return os.path.isdir(directryPath)   
 
+    #サーバーにディレクトリを作成
+    def makeDirectry(self, directryName):
+        self.ftp = FTP(host, username, password)
+        return self.ftp.mkd(directryName)
 
-# ======================
+    #未処理のファイル・ディレクトリが存在する
+    def unUploadFilesOrDirectries(self, func):
+        #self.getAll = os.walk(basepath)
+        print basepath
+        return self
+
+
+
+# ================================
+#   Function call
+# ================================
 
 print ftp().connect().ftp # OK 
 print ftp().getAllFilesAndDirectries().getAll # OK 
-print ftp().isDirectry('hogehoge/') # directry does not exits 
-print ftp().isDirectry('hogtext.txt') # file exits(not directry)
-print ftp().isDirectry(basepath) # directry exits
+print ftp().isDirectry('hogehoge/') # directry does not exits → OK
+print ftp().isDirectry('hogtext.txt') # file exits(not directry) → OK
+print ftp().isDirectry('./upload/') # directry exits → OK
+#print ftp().makeDirectry('dir') # OK
+print ftp().unUploadFilesOrDirectries(ftp().isDirectry(basepath)) # NG 
+#print ftp().unUploadFilesOrDirectries() # NG 
 
+# ================================
+#   Comment to make functions
+# ================================
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# ======================
-
-            #サーバーにディレクトリを作成
-        #未処理のファイル・ディレクトリが存在する
+            # (1)の次ループへ
     #(2)クライアントのアップロード対象のファイル・ディレクトリを全て取得
         #取得したものがファイルである
             #サーバーにファイルをアップロード
         #未処理のファイル・ディレクトリが存在する
+            # (2)の次ループへ
 #FTP接続を終了
 
